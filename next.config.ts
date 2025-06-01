57d814d0f1d1fc1912d5bc22a30a4f83d0f40f68
unknown <sergey1991serg@gmail.com>
2025-06-01 12:24:18 +0300

    Add GitHub Actions CI/CD workflow

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  basePath: '/portal', // ЦЕЙ РЯДОК МАЄ БУТИ ТУТ!
-
-  
-  // Додайте або перевірте цей блок, якщо у вас є статичні зображення/файли в public/
-  images: {
-    unoptimized: true, // Рекомендовано для складних розгортань, якщо ви не використовуєте Next.js Image Optimization на сервері
-  },
-
-  // Якщо ви використовуєте app/router або експериментальні фічі
-  experimental: {
-    appDir: true, // Якщо використовуєте App Router
-    // ... інші ваші експериментальні фічі
-  },
-
-  // ... інші ваші налаштування
+  basePath: '/portal',
+  assetPrefix: '/portal/.next/'
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import './fix-next-data-requests';
 
 export default function Home() {
   return (
