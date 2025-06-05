5ee1f8403980d722bef4bb87dc1b4c1bc2f51fdf
unknown <sergey1991serg@gmail.com>
2025-06-05 18:35:02 +0300

    add chat service

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
-@Controller('api') 
+@Controller('api') // Должно быть так
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get() 
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Get() // Должно быть так
+  getHello(): string { return 'Hello World!'; }
 }
--- chat-service/src/app.controller.ts
+++ chat-service/src/app.controller.ts
@@ -1,12 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
-@Controller()
+@Controller('chat') // ИЗМЕНИТЬ НА ЭТО!
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Get() // Должно быть так
+  getHello(): string { return 'Hello from Chat Service!'; }
 }
