974b797e631000815e1f32511313d32483d04beb
unknown <sergey1991serg@gmail.com>
2025-06-05 19:39:21 +0300

    Re-trigger deploy after fixing remote URL

--- chat-service/src/app.controller.ts
+++ chat-service/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('chat') 
+@Controller('chat') // <-- ИЗМЕНИТЬ НА ЭТО!
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // Это теперь будет обрабатывать /chat/
   getHello(): string {
     return this.appService.getHello();
   }
