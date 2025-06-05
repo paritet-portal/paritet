653fc4a2cf5b3e6304abd07d7e0f685b03d516ff
unknown <sergey1991serg@gmail.com>
2025-06-05 19:14:24 +0300

    add chat service

--- chat-service/src/app.controller.ts
+++ chat-service/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('chat') // <-- УБЕДИТЕСЬ, ЧТО ЭТА СТРОКА ИМЕННО ТАКАЯ
+@Controller('chat') 
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
