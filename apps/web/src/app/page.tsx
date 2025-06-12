f2ce786b73eb146771c7d85826c3e4d4a0a73ff5
unknown <sergey1991serg@gmail.com>
2025-06-12 19:01:11 +0300

    chore: Update Dockerfiles ----

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            SERGIUSss
+           WEB - SERGIUSss WEB - SERGIUSss WEB - SERGIUSss
           </li>
         </ol>
 
